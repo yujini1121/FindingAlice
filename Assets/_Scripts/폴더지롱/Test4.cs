be13bb4bea8d2b166f2026cc206f6cb7e1337de8
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2023-05-26 23:11:54 +0900

    .

---
+++ Assets/_Scripts/폴더지롱/Test4.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test4 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // ³¢¾ß¾Ç
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
