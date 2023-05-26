c1c59c4c228ffd3bd3c1582f0d0731904b291571
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2023-05-26 23:07:21 +0900

    .

---
+++ Assets/_Scripts/Test2.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 이것도 한글 주석이지롱
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/_Scripts/폴더지롱/test3.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // 집에보내주세요
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
