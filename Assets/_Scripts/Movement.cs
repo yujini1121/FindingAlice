7f334bb94c3e6258827c7040280700e4f8b0230a
SeungminYeom <81202999+SeungminYeom@users.noreply.github.com>
2023-05-26 21:56:35 +0900

    Movement 스크립트 수정

--- Assets/_Scripts/Movement.cs
+++ Assets/_Scripts/Movement.cs
@@ -8,7 +8,7 @@ public class Movement : MonoBehaviour
     [SerializeField] float jumpForce;
 
     Rigidbody       rigid;
-    CapsuleCollider capCollider;
+    bool collideToWall;
 
     float xAxis;
     [SerializeField] bool doJump;
@@ -17,7 +17,6 @@ public class Movement : MonoBehaviour
     void Start()
     {
         rigid       = GetComponent<Rigidbody>();
-        capCollider = GetComponent<CapsuleCollider>();
     }
 
     void Update()
@@ -37,7 +36,7 @@ public class Movement : MonoBehaviour
         Jump();
     }
 
-    void Move()
+    public void Move()
     {
         if (transform.localScale.x < 0 && xAxis > 0)
         {
@@ -58,41 +57,75 @@ public class Movement : MonoBehaviour
         rigid.velocity = new Vector3(velocity.x, rigid.velocity.y, velocity.z);
     }
 
-    void Jump()
+    public void Jump()
     {
         if (!doJump) return;
 
         doJump = false;
+        rigid.velocity = Vector3.zero;
         rigid.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 
-    void OnCollisionEnter(Collision collision)
+    // ===============================================================================================
+    // 플랫폼의 수직면에 닿았는지 확인하는 함수
+    // 플레이어에서 충돌점으로 향하는 벡터(collision.contacts[0].point - transform.position)와
+    // (0, -1, 0) 벡터를 내적하여 플랫폼과 플레이어의 충돌 상태를 구함.
+    // 0.75는 임의의 값. 추후 값 조정 예정
+    // 올바른 충돌 시 SmoothJump 코루틴을 멈추고, 다시 점프할 수 있는 상태로 전환
+    // 내적을 사용하여 계산한 이유 : collision.contacts[n].normal.y로 계산 시 플랫폼의 아래에서
+    // 플레이어가 점프하여 정수리를 닿아도 올바른 충돌로 인식하기 때문에 오직 플레이어의 발로부터
+    // 일정 각도의 충돌만 처리하기 위함
+    // ===============================================================================================
+    private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.tag == "Platform")
         {
+            if (Vector3.Dot(collision.contacts[0].point - transform.position, Vector3.down) > 0.75f)
+            {
+                isJump = false;
+                StopCoroutine(SmoothJump());
+            }
+        }
+    }
+
+    // ===============================================================================================
+    // 플랫폼의 벽 부분에 닿았는지 검사하는 함수
+    // 한번이라도 벽에 충돌했다면 collideToWall 변수를 True로 만듦.
+    // 반복문을 사용하여 검사하는 이유는 동시에 여러 물체와 충돌 상태가 될 수 있기 때문.
+    // ===============================================================================================
+    private void OnCollisionStay(Collision collision)
+    {
+        if (collision.gameObject.tag == "Platform" && !collideToWall)
+        {
             for (int i = 0; i < collision.contacts.Length; i++)
             {
-                if (collision.contacts[i].normal.y > 0.4f)
+                if (Vector3.Dot(collision.contacts[i].point - transform.position, Vector3.down) <= 0.75f)
                 {
-                    isJump = false;
+                    collideToWall = true;
                     break;
                 }
             }
         }
     }
 
-    void OnCollisionExit(Collision collision)
+    // ===============================================================================================
+    // 플랫폼의 바닥에서 벗어날 때만 호출되는 함수
+    // 벽에서 벗어났다면 호출되지 않으며, 플랫폼에서 점프하지 않고 미끄러 떨어진 상태라면 코루틴 실행하여
+    // 일정 시간 뒤에 점프 불가 상태로 전환
+    // ===============================================================================================
+    private void OnCollisionExit(Collision collision)
     {
-        if (collision.gameObject.tag == "Platform")
+        if (collision.gameObject.tag == "Platform" && !collideToWall)
         {
-            if (collision.contacts[0].normal.y < 0.4f)
-                return;
-
             StartCoroutine(SmoothJump());
         }
     }
 
-    IEnumerator SmoothJump()
+    // ===============================================================================================
+    // 부드러운 점프 구현하기 위한 함수
+    // 추후 함수에 있는 값 분리
+    // ===============================================================================================
+    private IEnumerator SmoothJump()
     {
         yield return new WaitForSeconds(0.2f);
         isJump = true;
